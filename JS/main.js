08cd5ff8f6718e2e62a3c930ccad5b7d261ba4b1
Flamedark12 <hanscristophergoytia@gmail.com>
2021-11-28 16:49:34 -0500

    Updated

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+const menu  = document.getElementById("menu");
+const btnmenu = document.getElementById("btnmenu");
+
+btnmenu.addEventListener("click",function(){
+
+    menu.classList.toggle("active");
+
+})
+
